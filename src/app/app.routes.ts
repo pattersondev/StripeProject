5ac41e7e866984ec91363af92c77df9b5a81ccf1
Sam Patterson <pattersonrsam@gmail.com>
2024-06-11 15:49:40 -0400

    Banner and navbar skeleton done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ProductViewComponent } from './components/product-view/product-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: HomeComponent,
+    children: [
+        {
+            path: 'checkout/:id',
+            component: CheckoutComponent
+        },
+        {
+            path: 'product/:id',
+            component: ProductViewComponent
+        }
+    ]
+}];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from '../banner/banner.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
