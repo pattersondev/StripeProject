75c57914f422624113c5848225d1fd7da7915076
Sam Patterson <pattersonrsam@gmail.com>
2024-06-14 11:29:16 -0400

    small cleanup

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://localhost:3000',
-    priceId: 'price_1JGJfzHbYRPyPKlvz1Z2ZQ1D',
-    stripeKey: 'pk_test_9HJFqFbhYRPyPKlvODTMhyit'
-}
