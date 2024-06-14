1697a7eaac6936b8379c166cd52d2249c5787481
Sam Patterson <pattersonrsam@gmail.com>
2024-06-14 11:26:53 -0400

    fix deployment on frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://boiling-sea-08841-49d3e82f32ee.herokuapp.com/',
+    apiUrl: 'https://boiling-sea-08841-49d3e82f32ee.herokuapp.com',
     priceId: 'price_1PR1fPGOCNomBDozKezqiNNc',
     stripeKey: 'pk_test_9HJFqFbhYRPyPKlvODTMhyit'
 }
